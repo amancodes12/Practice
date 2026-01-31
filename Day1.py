bf843e5b42d89224d359ef1bfb4133f3b25eb345
Aman <amannn1295@gmail.com>
2026-01-31 22:44:42 +0530

    Add factorial and electricity bill functions

--- Day1.py
+++ Day1.py
@@ -20,3 +20,28 @@ def is_armstrong(n):
   power = len(digits)
   total = sum(int(d)**power for d in digits)
   return total == n
+
+
+#4. Factorial
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  return n * factorial(n-1)
+
+num = int(input("Enter a number: "))
+print("Factorial of", num, "is", factorial(num))
+
+
+#5. Fibonnaci
+
+
+#6. Electricity Bill
+def e_bill(units):
+  if units <= 100:
+    return units*1
+  elif units <= 200:
+    return 100*1 + (units-100)*3
+  else:
+    return 100*1 + 100*3 + (units-200)*5
+units = int(input("Enter no. of units: "))
+print("Electricity bill is:", e_bill(units))
