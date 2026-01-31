95af10e59cc48578fd1d09fd3eaee1fed20ba649
Aman <amannn1295@gmail.com>
2026-01-31 22:23:09 +0530

    Implement Armstrong number check function
    Added a function to check if a number is Armstrong.

--- Day1.py
+++ Day1.py
@@ -5,9 +5,18 @@ if str(num) == str(num) [::-1]:
 else
   print("Not Palindrome")
 
+
 #2. String Palindrome
 s = input("Enter a string: ")
 if s == s [::-1]:
   print("Palindrome")
 else:
   print("Not Palindrome")
+
+
+#3. Check if a number is Armstrong 
+def is_armstrong(n):
+  digits = str(n)
+  power = len(digits)
+  total = sum(int(d)**power for d in digits)
+  return total == n
